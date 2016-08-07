a0101936727576662e89656136c9e186e39d9a3d
林達也 <t.hayashi0077+github@gmail.com>
2016-08-07 19:07:20 +0900

    Initial Commit

---
+++ Statistics/Statistics.h
@@ -0,0 +1,19 @@
+//
+//  Statistics.h
+//  Statistics
+//
+//  Created by 林達也 on 2016/08/07.
+//  Copyright © 2016年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Statistics.
+FOUNDATION_EXPORT double StatisticsVersionNumber;
+
+//! Project version string for Statistics.
+FOUNDATION_EXPORT const unsigned char StatisticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Statistics/PublicHeader.h>
+
+
